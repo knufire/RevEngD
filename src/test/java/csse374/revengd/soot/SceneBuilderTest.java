95129f41368b5851b4e0d7c6586a3108083befa9
Rahul Yarlagadda <yarlagrt@rose-hulman.edu>
2017-12-03 16:09:11 -0500

    Removed tests referencing deleted example calculator app.

--- src/test/java/csse374/revengd/soot/SceneBuilderTest.java
+++ src/test/java/csse374/revengd/soot/SceneBuilderTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,36 +14,14 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 import csse374.revengd.soot.SceneBuilder;
-import soot.Scene;
-import soot.SootClass;
-import soot.SootMethod;
 
 public class SceneBuilderTest {
 	SceneBuilder builder;
-	List<String> allFixtureClasses;
-	List<String> allRechableClassesFromEntry;
-	List<String> allUnRechableClassesFromEntry;
-
+	
 	@Before
 	public void setUp() throws Exception {
 		this.builder = SceneBuilder.create();
 		
-		this.allRechableClassesFromEntry = Arrays.asList(
-				"csse374.revengd.examples.fixtures.CalculatorApp",
-				"csse374.revengd.examples.fixtures.CalculatorA",
-				"csse374.revengd.examples.fixtures.CalculatorB",
-				"csse374.revengd.examples.fixtures.ICalculator"
-		);
-		
-		this.allUnRechableClassesFromEntry = Arrays.asList(
-				"csse374.revengd.examples.fixtures.CalculatorC",
-				"csse374.revengd.examples.fixtures.UnrelatedClass"				
-		);
-		
-		this.allFixtureClasses = new ArrayList<>();
-		this.allFixtureClasses.addAll(this.allRechableClassesFromEntry);
-		this.allFixtureClasses.addAll(this.allUnRechableClassesFromEntry);
-
 		// Needed for running tests from Gradle
 		Path buildResourcesTestPath = Paths.get("build", "resources", "test");
 		if(!buildResourcesTestPath.toFile().exists()) {
@@ -148,104 +125,5 @@ public class SceneBuilderTest {
 		
 		assertEquals(exclusions, builder.exclusions);
 	}
-	
-	@Test
-	public void testShouldLoadClassesOnlyRechableFromMain() {
-		final Scene scene = SceneBuilder.create()
-				.addExclusions(Arrays.asList("sun.*", "soot.*", "polygot.*"))
-				.setEntryClass("csse374.revengd.examples.fixtures.CalculatorApp")
-				.addEntryPointMatcher(new MainMethodMatcher("csse374.revengd.examples.fixtures.CalculatorApp"))
-				.build();
 
-		this.allRechableClassesFromEntry.forEach(clazz -> {
-			assertTrue(scene.containsClass(clazz));
-		});
-
-		this.allUnRechableClassesFromEntry.forEach(clazz -> {
-			assertFalse(scene.containsClass(clazz));
-		});
-	}
-	
-	@Test
-	public void testShouldLoadClassesRechableFromMainAndManuallyLoadedClasses() {
-		final Scene scene = SceneBuilder.create()
-				.addExclusions(Arrays.asList("sun.*", "soot.*", "polygot.*"))
-				.setEntryClass("csse374.revengd.examples.fixtures.CalculatorApp")
-				.addClass("csse374.revengd.examples.fixtures.CalculatorC")
-				.addClass("csse374.revengd.examples.fixtures.UnrelatedClass")
-				.addEntryPointMatcher(new MainMethodMatcher("csse374.revengd.examples.fixtures.CalculatorApp"))
-				.build();
-
-		this.allFixtureClasses.forEach(clazz -> {
-			assertTrue(scene.containsClass(clazz));
-		});
-	}	
-
-	@Test
-	public void testShouldLoadClassesFromJDK() {
-		final String jComponentName = "javax.swing.JComponent";
-		final Scene scene = SceneBuilder.create()
-				.addExclusions(Arrays.asList("soot.*", "polygot.*"))
-				.setEntryClass("csse374.revengd.examples.fixtures.CalculatorApp")
-				.addClass(jComponentName)
-				.addEntryPointMatcher(new MainMethodMatcher("csse374.revengd.examples.fixtures.CalculatorApp"))
-				.build();
-
-		// Check if the JComponent is loaded successfully
-		SootClass jComponentClass = scene.getSootClass(jComponentName);
-		assertNotNull(jComponentClass);
-		
-		// Check if the "void paint(Graphics)" method is present
-		SootMethod method = jComponentClass.getMethodByName("paint");
-		assertTrue(method.isPublic());
-		assertEquals(method.getParameterCount(), 1);
-		
-		// Check if the body of the paint method is also loaded
-		assertNotNull(method.retrieveActiveBody());
-		
-		// Check if the hierarchy information is preserved
-		SootClass containerClass = jComponentClass.getSuperclass();
-		assertNotNull(containerClass);
-		
-		// Check if the "void getComponent(int)" method is present in the Container class
-		method = containerClass.getMethodByName("getComponent");
-		assertTrue(method.isPublic());
-		assertEquals(method.getParameterCount(), 1);
-				
-		// Check that the fields of the java.awt.Container class is also loaded
-		// Looks up the layoutMgr field of the Container class
-		assertNotNull(containerClass.getFieldByName("layoutMgr"));
-		
-		// Check that the super class of the super is also loaded correctly
-		assertEquals("java.awt.Component", containerClass.getSuperclass().getName());
-		
-		// At this point it would be safe to conclude that the JDK class can be loaded correctly!
-	}
-
-	@Test
-	public void testProcessDirShouldAnalyzeAllClassesInDir() {
-		Path path = Paths.get("build", "classes", "main");
-		if(!path.toFile().exists()) {
-			path = Paths.get("bin");
-			
-			if(!path.toFile().exists()) {
-				fail("Could not find the classes directory - [" + path.toAbsolutePath().toString() + "]");
-			}
-		}
-		
-		String dirToProcess = path.toAbsolutePath().toString();
-		System.out.println("Processing test analysis from: " + dirToProcess);
-		
-		final Scene scene = SceneBuilder.create()
-				.addExclusions(Arrays.asList("sun.*", "soot.*", "polygot.*"))
-				.setEntryClass("csse374.revengd.examples.fixtures.CalculatorApp")
-				.addEntryPointMatcher(new MainMethodMatcher("csse374.revengd.examples.fixtures.CalculatorApp"))
-				.addDirectory(path.toAbsolutePath().toString())
-				.build();
-
-		
-		this.allFixtureClasses.forEach(clazz -> {
-			assertTrue(scene.containsClass(clazz));
-		});
-	}
 }
