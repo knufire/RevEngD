cfbb716128b0d0a4f2a8890e98498defb06135d9
cKeinsley <keinsleyChris@gmail.com>
2018-01-25 13:06:17 -0500

    updated project to reflect classes from other project

--- src/main/java/odyssey/app/Runner.java
+++ src/main/java/odyssey/app/Runner.java
@@ -3,16 +3,22 @@ package odyssey.app;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import csse374.revengd.examples.fixtures.ICalculator;
 import odyssey.modules.PipelineModule;
 import odyssey.modules.ReflectionModule;
+import polyglot.ext.jl.ast.Special_c;
 
 public class Runner {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
     @SuppressWarnings("unchecked")
     Class<? extends ICalculator> clazz = (Class<? extends ICalculator>) Class
         .forName("csse374.revengd.detectors.SpecialCalculatorC");
+    
+    
     ICalculator calculator = clazz.newInstance();
+    
+    
     System.out.println("Class loaded successfully! Result: " + calculator.multiply(1, 2, 3));
 
     PropertiesSetter.set(args);
