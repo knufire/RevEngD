2b2f5cd7ac97b943f40adfcce769f978650a930f
cKeinsley <keinsleyChris@gmail.com>
2018-01-25 12:52:43 -0500

    updated gradle file

--- src/main/java/odyssey/app/Runner.java
+++ src/main/java/odyssey/app/Runner.java
@@ -9,15 +9,17 @@ import odyssey.modules.ReflectionModule;
 public class Runner {
 
   public static void main(String[] args) {
-    
+    @SuppressWarnings("unchecked")
+    Class<? extends ICalculator> clazz = (Class<? extends ICalculator>) Class
+        .forName("csse374.revengd.detectors.SpecialCalculatorC");
+    ICalculator calculator = clazz.newInstance();
+    System.out.println("Class loaded successfully! Result: " + calculator.multiply(1, 2, 3));
+
     PropertiesSetter.set(args);
-     Injector injector = Guice.createInjector(new PipelineModule(), new ReflectionModule());
-     Processor p = injector.getInstance(Processor.class);
-     p.executePipeline();
+    Injector injector = Guice.createInjector(new PipelineModule(), new ReflectionModule());
+    Processor p = injector.getInstance(Processor.class);
+    p.executePipeline();
 
   }
-  
-
-  
 
 }
