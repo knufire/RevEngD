9e5dc4311859b594a143272412fbb2f6de9ad5e5
Rahul Yarlagadda <knufire@gmail.com>
2018-02-07 16:59:03 -0500

    Possibly fixed IoverC analyzer.

--- src/main/java/odyssey/analyzers/InheritanceOverCompositionAnalyzer.java
+++ src/main/java/odyssey/analyzers/InheritanceOverCompositionAnalyzer.java
@@ -1,11 +1,19 @@
 package odyssey.analyzers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import odyssey.filters.Filter;
 import odyssey.models.Pattern;
 import odyssey.models.Relation;
 import odyssey.models.Relationship;
+import soot.Body;
+import soot.SootClass;
+import soot.SootMethod;
+import soot.Unit;
+import soot.jimple.InvokeStmt;
+import soot.toolkits.graph.ExceptionalUnitGraph;
+import soot.toolkits.graph.UnitGraph;
 
 public class InheritanceOverCompositionAnalyzer extends Analyzer {
 
@@ -20,17 +28,51 @@ public class InheritanceOverCompositionAnalyzer extends Analyzer {
     checkRelationships(patterns, relationships);
     return bundle;
   }
-  
+
   private void checkRelationships(List<Pattern> patterns, List<Relationship> relationships) {
     Pattern p = new Pattern("IoverC");
-    for (Relationship r: relationships) {
-      if (r.getRelation().equals(Relation.EXTENDS)) {
-        p.put("IoverC", r);
-        p.put("IoverC", r.getFromClass());
-        p.put("IoverC", r.getToClass());
+    for (Relationship r : relationships) {
+      if (r.getRelation().equals(Relation.EXTENDS) && isViolation(r)) {
+        addPattern(r, p);
       }
     }
     patterns.add(p);
+  }
+
+  private boolean isViolation(Relationship r) {
+    SootClass superClass = r.getToClass();
+    SootClass subClass = r.getFromClass();
+    List<SootMethod> overriddenMethods = getOverriddenMethods(superClass, subClass);
+    for (SootMethod m : overriddenMethods) {
+      if (!delagatesCall(m)) return true;
+    }
+    return false;
+  }
+
+  private boolean delagatesCall(SootMethod m) {
+    if (!m.hasActiveBody()) return true;
+    Body body = m.getActiveBody();
+    UnitGraph graph = new ExceptionalUnitGraph(body);
+    for (Unit u : graph) {
+      if (u instanceof InvokeStmt) {
+        InvokeStmt stmt = (InvokeStmt) u;
+        if (stmt.containsFieldRef()) return true;
+      }
+    }
+    return false;
+  }
+
+  private List<SootMethod> getOverriddenMethods(SootClass superClass, SootClass subClass) {
+    List<SootMethod> result = new ArrayList<>();
+    result.addAll(subClass.getMethods());
+    result.retainAll(superClass.getMethods());
+    return result;
+  }
+
+  private void addPattern(Relationship r, Pattern p) {
+    p.put("IoverC", r);
+    p.put("IoverC", r.getFromClass());
+    p.put("IoverC", r.getToClass());
   }
 
 }
