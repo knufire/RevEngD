5a4581e147c74314954f5c61289d168ac39a3511
Rahul Yarlagadda <knufire@gmail.com>
2018-02-07 14:29:41 -0500

    ROund 8

--- src/main/java/odyssey/renderers/RelationshipRenderer.java
+++ src/main/java/odyssey/renderers/RelationshipRenderer.java
@@ -12,11 +12,11 @@ public class RelationshipRenderer implements IRelationshipRenderer {
   @Override
   public final String render(Relationship t) {
     StringBuilder builder = new StringBuilder();
-    builder.append(renderLeft(!isReverse(t.getRelation()) ? t.getFromClass() : t.getToClass()));
+    builder.append(renderLeft(isReverse(t.getRelation()) ? t.getFromClass() : t.getToClass()));
     builder.append(" ");
     builder.append(renderArrow(t.getRelation(), t.getCardinality()));
     builder.append(" ");
-    builder.append(renderRight(!isReverse(t.getRelation()) ? t.getToClass() : t.getFromClass()));
+    builder.append(renderRight(isReverse(t.getRelation()) ? t.getToClass() : t.getFromClass()));
     return builder.toString();
   }
 
@@ -39,7 +39,7 @@ public class RelationshipRenderer implements IRelationshipRenderer {
   @Override
   public String renderArrow(Relation relation, int cardinality) {
     StringBuilder builder = new StringBuilder();
-    if (!isReverse(relation)) {
+    if (isReverse(relation)) {
       builder.append(parseCardinality(cardinality));
       builder.append(" ");
       builder.append(parseArrow(relation));
